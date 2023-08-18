bba8b902e08f224fc9771689e3644483ffd29ffa
HarishKathir <harishbabu1532002@gmail.com>
2023-08-18 22:14:24 +0530

    Initial Commit

---
+++ FOF/FOFApp.swift
@@ -0,0 +1,17 @@
+//
+//  FOFApp.swift
+//  FOF
+//
+//  Created by Harishkathir on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FOFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
